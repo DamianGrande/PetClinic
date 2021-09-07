92cf11ff1bac9aade59de7532b9ac8605bbc864d
Damian Grande <damian.grande+1-at-980079093348@alimentiamoci.com>
2021-09-07 10:10:20 +0200

    Display Owner Record.

--- pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/controllers/OwnerControllerTest.java
+++ pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/controllers/OwnerControllerTest.java
@@ -14,7 +14,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 import static org.hamcrest.Matchers.hasSize;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.verifyZeroInteractions;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -56,5 +56,11 @@ class OwnerControllerTest {
     void findOwners() throws Exception {
         this.mockMvc.perform(get("/owners/find")).andExpect(status().isOk()).andExpect(view().name("nothingImplemented"));
         verifyZeroInteractions(this.ownerService);
+    }
+
+    @Test
+    void displayOwner() throws Exception {
+        when(this.ownerService.findById(anyLong())).thenReturn(Owner.builder().id(1L).build());
+        this.mockMvc.perform(get("/owners/123")).andExpect(status().isOk()).andExpect(view().name("owners/ownerDetails")).andExpect(model().attributeExists("owner"));
     }
 }
